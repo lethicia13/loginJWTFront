4f67218e69a8842dc21a37ea66a09921fc24f68f
Lethicia <lethiciamsm@gmail.com>
2025-11-21 17:41:27 -0300

    começando a implementar jwt

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Usuario } from '../../auth/usuario';
+import { LoginService } from '../../auth/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
 export class CadastroComponent {
+  usuario: Usuario = {
+    id: 0,
+    username: '',
+    password: '',
+    role: 'USER',
+  };
 
+  loginService = inject(LoginService);
+  router = inject(Router);
+
+  cadastrar() {
+    if (!this.usuario.username || !this.usuario.password) {
+      alert('Por favor, preencha todos os campos obrigatórios.');
+      return;
+    }
+
+    this.loginService.cadastrar(this.usuario).subscribe({
+      next: (response) => {
+        alert('Cadastro realizado com sucesso! Faça login.');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        alert(
+          'Erro ao cadastrar usuário: ' +
+            (err.error.message || 'Verifique o console.')
+        );
+        console.error(err);
+      },
+    });
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
+import { Login } from '../../auth/login';
+import { LoginService } from '../../auth/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  login: Login = {
+    username: '',
+    password: ''
+  };
 
+  loginService = inject(LoginService);
+  router = inject(Router);
+
+  logar() {
+    if (!this.login.username || !this.login.password) {
+      alert('Por favor, preencha o usuário e a senha.');
+      return;
+    }
+
+    this.loginService.logar(this.login).subscribe({
+      next: (token) => {
+        this.loginService.addToken(token);
+        alert('Login realizado com sucesso!');
+        this.router.navigate(['/home']); 
+      },
+      error: (err) => {
+        alert('Falha no login. Verifique as credenciais.');
+        console.error(err);
+      }
+    });
+  }
 }
